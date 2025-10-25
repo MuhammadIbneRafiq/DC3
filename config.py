f7e3bbdbef7902502740a067648987d330dc0d68
jhaasbroek <j.hattingh.haasbroek@student.tue.nl>
2025-10-25 21:35:43 +0200

    Add more configs

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 __RANDOM_STATE__ = 42
 __n_SPLITS__ = 5  # for k-fold
 __EPOCHS__ = 50
-input_path = "data/unclustered_none"  # path to directory containing the `images`, `masks_bleached` and `masks_non_blached` folers
+input_path = "data"
 output_path = "data"  # path to store folder containing clustered and filtered images
 root = "data/cluster_2"  # TODO: specify dataset to run model on
+MODEL_NAME = "nvidia/mit-b2"
+VIS_DIR = "assets"
